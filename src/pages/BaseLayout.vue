430ab5662e8bc752f3d0e9a872c03777a150f9ac
Deniz <denizderezzed@gmail.com>
2022-06-05 09:34:08 +0200

    BaseLayout style

--- src/pages/BaseLayout.vue
+++ src/pages/BaseLayout.vue
@@ -1,27 +1,40 @@
 <template>
-  <el-menu
-      :default-active="activeIndex"
-      class="el-menu-demo"
-      mode="horizontal"
-      @select="handleSelect"
-    >
-      <el-menu-item index="1">Processing Center</el-menu-item>
-      <el-menu-item index="3">Info</el-menu-item>
-      <el-menu-item index="4">Orders</el-menu-item>
-    </el-menu>
+  <el-container class="container">
+    <el-header class="header">
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+      >
+        <el-menu-item index="1">Processing Center</el-menu-item>
+        <el-menu-item index="3">Info</el-menu-item>
+        <el-menu-item index="4">Orders</el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main class="main">Main</el-main>
+  </el-container>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 
-const activeIndex = ref('');
+const activeIndex = ref('')
 
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
 }
-
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.container {
+  max-width: 100%;
+  .header {
+    --ep-header-padding: 0;
+  }
+  .main {
+    display: flex;
+    --ep-main-padding: 20px;
+  }
+}
 </style>
