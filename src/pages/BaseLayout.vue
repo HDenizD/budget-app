931081cb90b59eec9c4ce100f206cfa345918081
Deniz <denizderezzed@gmail.com>
2022-06-05 15:48:34 +0200

    menu

--- src/pages/BaseLayout.vue
+++ src/pages/BaseLayout.vue
@@ -3,13 +3,15 @@
     <el-header class="header">
       <el-menu
         :default-active="activeIndex"
-        class="el-menu-demo"
+        class="menu"
         mode="horizontal"
+        :ellipsis="false"
         @select="handleSelect"
       >
-        <el-menu-item index="1">Processing Center</el-menu-item>
-        <el-menu-item index="3">Info</el-menu-item>
-        <el-menu-item index="4">Orders</el-menu-item>
+        <el-menu-item index="1">Dashboard</el-menu-item>
+        <el-menu-item index="2">User</el-menu-item>
+        <div class="flex-grow" />
+        <el-menu-item index="3">Settings</el-menu-item>
       </el-menu>
     </el-header>
     <el-main class="main">Main</el-main>
@@ -19,7 +21,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
-const activeIndex = ref('')
+const activeIndex = ref('1')
 
 const handleSelect = (key: string, keyPath: string[]) => {
   console.log(key, keyPath)
@@ -31,6 +33,9 @@ const handleSelect = (key: string, keyPath: string[]) => {
   max-width: 100%;
   .header {
     --ep-header-padding: 0;
+    .menu {
+      display: flex;
+    }
   }
   .main {
     display: flex;
