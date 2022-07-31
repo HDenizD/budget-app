eb1ba7471b66609c2877580ac68d30180d05d9e7
Deniz <denizderezzed@gmail.com>
2022-07-31 22:42:11 +0200

    WIP: login test

---
+++ src/tests/Login.test.ts
@@ -0,0 +1,23 @@
+import { mount } from '@vue/test-utils'
+import Login from '@/modules/auth/pages/Login.vue'
+import { createTestingPinia } from '@pinia/testing'
+
+test('login with correct credentials', async () => {
+  // expect(Login).toBeTruthy();
+  const wrapper = mount(Login, {
+    global: {
+      plugins: [createTestingPinia()],
+    },
+  })
+
+  const loginCredentials = {
+    username: 'test@lala.de',
+    password: 'test1234',
+  }
+
+  const loginButton = wrapper.find('.p-button-success')
+  loginButton.trigger('click')
+
+
+  expect(loginButton).toHaveBeenCalled()
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 // import { fileURLToPath, URL } from "url";
 const path = require('path');
@@ -12,6 +12,10 @@ export default defineConfig({
   },
   server: {
     host: true
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
   },
   plugins: [vue()]
 })
